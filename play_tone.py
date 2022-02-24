b85375619f4cabebeb22e359cddf55c6124d60d4
lxzhe718 <lxzhe718@uw.edu>
2022-02-25 07:34:17 +0800

    test

---
+++ play_tone.py
@@ -0,0 +1,20 @@
+import play_hole as ph
+def play_c():
+  ph.close_hole1()
+  ph.close_hole2()
+  ph.close_hole3()
+  ph.close_hole5()
+  ph.close_hole6()
+
+def play_c_sharp():
+  ph.close_hole1()
+  ph.close_hole2()
+  ph.close_hole3()
+  ph.close_hole5()
+  ph.close_half_hole6()
+
+def play_d():
+  ph.close_hole1()
+  ph.close_hole2()
+  ph.close_hole3()
+  ph.close_hole5()
